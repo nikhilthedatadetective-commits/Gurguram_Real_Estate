a52fe37f1d559015ccec960b8fdc1f5ca57af57c
Nikhil Kumar Singh <nikhil.thedatadetective@gmail.com>
2025-09-12 13:04:43 +0530

    Initial commit with Streamlit app and pipeline_compressed.pkl

---
+++ home_app.py
@@ -0,0 +1,6 @@
+import streamlit as st
+
+st.set_page_config(page_title="My Multipage Website", layout="wide")
+
+st.title("Welcome to My Multipage Website")
+st.write("Use the sidebar to navigate to other pages 🚀")
